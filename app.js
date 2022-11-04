3fe8634c94df264491a3abe17c5a44f05abfcd3a
Maksym_enKO <ingool.ua@gmail.com>
2022-11-04 20:34:29 +0200

    listener on button

--- app.js
+++ app.js
@@ -1,39 +1,85 @@
-const tl = gsap.timeline({
-  defaults: { duration: 0.75, ease: 'Power3.easeOut' },
-});
-
-tl.fromTo(
-  '.hero-img',
-  { scale: 1.3, borderRadius: '0rem' },
-  {
-    scale: 1,
-    borderRadius: '2rem',
-    delay: 0.35,
-    duration: 2.5,
-    ease: 'elastic.out(1.5, 1)',
-  }
-);
-tl.fromTo('.cta1', { x: '100%', opacity: 0.5 }, { x: 0, opacity: 1 }, '<20%');
-tl.fromTo('.cta2', { y: '100%', opacity: 0.5 }, { y: 0, opacity: 1 }, '<20%');
-tl.fromTo('.cta3', { x: '-100%', opacity: 0.5 }, { x: 0, opacity: 1 }, '<20%');
-tl.fromTo('.cta4', { x: '100%', opacity: 0.5 }, { x: 0, opacity: 1 }, '<20%');
-tl.fromTo('.cta5', { y: '100%', opacity: 0.5 }, { y: 0, opacity: 1 }, '<20%');
-tl.fromTo('.cta6', { x: '100%', opacity: 0.5 }, { x: 0, opacity: 1 }, '<20%');
-tl.fromTo('.cta-btn', { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, '<');
-
-//Split text alternative
-const logo = document.querySelector('.logo');
-const letters = logo.textContent.split('');
-
-logo.textContent = '';
-
-for (let letter of letters) {
-  logo.innerHTML += `<span class=\"letter\">${letter}</span>`;
+function textAnimation(timeLine) {
+  timeLine.fromTo(
+    '.hero-img',
+    { scale: 1.3, borderRadius: '0rem' },
+    {
+      scale: 1,
+      borderRadius: '2rem',
+      delay: 0.35,
+      duration: 2.5,
+      ease: 'elastic.out(1.5, 1)',
+    }
+  );
+  timeLine.fromTo(
+    '.cta1',
+    { x: '100%', opacity: 0.5 },
+    { x: 0, opacity: 1 },
+    '<20%'
+  );
+  timeLine.fromTo(
+    '.cta2',
+    { y: '100%', opacity: 0.5 },
+    { y: 0, opacity: 1 },
+    '<20%'
+  );
+  timeLine.fromTo(
+    '.cta3',
+    { x: '-100%', opacity: 0.5 },
+    { x: 0, opacity: 1 },
+    '<20%'
+  );
+  timeLine.fromTo(
+    '.cta4',
+    { x: '100%', opacity: 0.5 },
+    { x: 0, opacity: 1 },
+    '<20%'
+  );
+  timeLine.fromTo(
+    '.cta5',
+    { y: '100%', opacity: 0.5 },
+    { y: 0, opacity: 1 },
+    '<20%'
+  );
+  timeLine.fromTo(
+    '.cta6',
+    { x: '100%', opacity: 0.5 },
+    { x: 0, opacity: 1 },
+    '<20%'
+  );
+  timeLine.fromTo('.cta-btn', { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, '<');
 }
 
-gsap.set('.letter', { display: 'inline-block' });
-gsap.fromTo(
-  '.letter',
-  { y: '100%' },
-  { y: 0, delay: 2, stagger: 0.05, ease: 'back.out(3)' }
-);
+function splitText(timeLine, letters, logo) {
+  logo.textContent = '';
+
+  for (let letter of letters) {
+    logo.innerHTML += `<span class=\"letter\">${letter}</span>`;
+  }
+
+  gsap.set('.letter', { display: 'inline-block' });
+  gsap.fromTo(
+    '.letter',
+    { y: '100%' },
+    { y: 0, delay: 2, stagger: 0.05, ease: 'back.out(3)' }
+  );
+}
+
+function ready() {
+  const tl = gsap.timeline({
+    defaults: { duration: 0.75, ease: 'Power3.easeOut' },
+  });
+  //Split text alternative
+  const logo = document.querySelector('.logo');
+  const letters = logo.textContent.split('');
+  const button = document.querySelector('.cta-btn');
+
+  textAnimation(tl);
+  splitText(tl, letters, logo);
+
+  button.addEventListener('click', () => {
+    textAnimation(tl);
+    splitText(tl, letters, logo);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', ready);
